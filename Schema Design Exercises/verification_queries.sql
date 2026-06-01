20b310132daa5c1125622646d9a7d45689d9c844
Hafiz Muhammad Salman <hafizmuhammadsalman13@gmail.com>
2026-05-31 19:54:18 -0700

    Create verification_queries.sql

---
+++ Schema Design Exercises/verification_queries.sql
@@ -0,0 +1,27 @@
+-- Query 1: List all courses with their instructor names
+SELECT 
+    -- TODO: Select appropriate columns
+FROM courses
+    -- TODO: Join with instructors table
+;
+
+-- Query 2: List all students enrolled in a specific course
+SELECT 
+    -- TODO: Select student information
+FROM students
+    -- TODO: Join through enrollments to courses
+WHERE 
+    -- TODO: Filter by course code
+;
+
+-- Query 3: Show student name, course title, and grade for all enrollments
+SELECT 
+    -- TODO: Select from multiple tables
+FROM 
+    -- TODO: Join students, enrollments, and courses
+;
+
+-- Query 4: Count how many students are enrolled in each course
+SELECT 
+    -- TODO: Use GROUP BY and COUNT
+;
