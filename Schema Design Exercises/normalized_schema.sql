660b6a89c1f8076f3521f892e393da29dcac59da
Hafiz Muhammad Salman <hafizmuhammadsalman13@gmail.com>
2026-05-31 19:53:07 -0700

    Create normalized_schema.sql

---
+++ Schema Design Exercises/normalized_schema.sql
@@ -0,0 +1,25 @@
+-- TODO: Create Students table
+CREATE TABLE students (
+    -- Define appropriate columns
+    -- Include primary key
+    -- Add constraints (NOT NULL, UNIQUE where appropriate)
+);
+
+-- TODO: Create Instructors table
+CREATE TABLE instructors (
+    -- Define appropriate columns
+);
+
+-- TODO: Create Courses table
+CREATE TABLE courses (
+    -- Define appropriate columns
+    -- Include foreign key to instructors
+);
+
+-- TODO: Create Enrollments table (junction table)
+CREATE TABLE enrollments (
+    -- Define appropriate columns
+    -- Include composite primary key or surrogate key
+    -- Include foreign keys to students and courses
+    -- Include semester and grade columns
+);
