5ab1308993e405afb6dfe224d0b5f4268a0107cb
Hafiz Muhammad Salman <hafizmuhammadsalman13@gmail.com>
2026-05-31 19:53:47 -0700

    Create  sample_data.sql

---
+++ Schema Design Exercises/sample_data.sql
@@ -0,0 +1,11 @@
+-- TODO: Insert at least 2 instructors
+INSERT INTO instructors VALUES (...);
+
+-- TODO: Insert at least 3 courses
+INSERT INTO courses VALUES (...);
+
+-- TODO: Insert at least 3 students
+INSERT INTO students VALUES (...);
+
+-- TODO: Insert at least 5 enrollment records
+INSERT INTO enrollments VALUES (...);
